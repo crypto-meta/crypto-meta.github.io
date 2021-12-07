07c8488771ee07d53441f48ce3937458912dcc0f
youngjuning <youngjuning@users.noreply.github.com>
2021-12-07 08:53:05 +0000

    deploy: crypto-meta/crypto-meta@948159986274c722dd82c14017643cb5bcb738fe

---
+++ _next/static/yKEjwXdajxh76US1geLIb/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/608-0e926fc2061fdbe3.js","static/css/d2cdcefd7d643c08.css","static/chunks/pages/index-36ebb960fbd1b6bd.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
