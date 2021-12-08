1393952de9ae61a116ba37e01e91d3f74418f2ee
youngjuning <youngjuning@users.noreply.github.com>
2021-12-08 03:05:42 +0000

    deploy: crypto-meta/crypto-meta@80d58b30b84aecd6874c50e433baf732def3babe

--- _next/static/95Lpqjc0u9rzLnsQbkNxG/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/795-019c9b334be58049.js","static/css/d2cdcefd7d643c08.css","static/chunks/pages/index-f927da60b12910f4.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/wnWGwixMsSgxNchs9q_Gk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/411-fdda82d687d5bf7c.js","static/css/d2cdcefd7d643c08.css","static/chunks/pages/index-f3bc79a4c856c858.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
