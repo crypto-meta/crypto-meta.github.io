5fea16c1ebbd1da44fd191eaf134dd92b13b2472
youngjuning <youngjuning@users.noreply.github.com>
2021-12-27 12:32:40 +0000

    deploy: crypto-meta/crypto-meta@ea2180d5407aa4dbbc52390d3e0e0d826dd45b32

---
+++ _next/static/chunks/pages/squid-nft-b67d59f16bfbc307.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[785],{225:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/squid-nft",function(){return u(1538)}])}},function(n){n.O(0,[305,538,774,888,179],(function(){return _=225,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
